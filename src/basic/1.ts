286987bdcc708ef677004f6386b7637ad942182f
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-10-22 21:14:10 +0300

    1

---
+++ src/basic/1.ts
@@ -0,0 +1,13 @@
+// Завдання 1
+
+// Виконуйте це завдання у файлі src/basic/1.ts.
+
+// Є наступний JavaScript код:
+
+// const age = 50;
+// const username = 'Max';
+// const toggle = true;
+// const empty = null;
+// const callback = (a) => { return 100 + a };
+
+// Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
